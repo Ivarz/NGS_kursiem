1aee5e0bf38744bfc22ba9a694e548af8b37a1dc
Ivarz <ivarssila@inbox.lv>
2015-02-24 21:41:03 +0200

    uzskates materiali pielikti

--- lekc/snp_calling/data/my_pipeline.sh
+++ lekc/snp_calling/data/my_pipeline.sh
@@ -1,15 +1,20 @@
+#Read mapping
 tmap map3 -f reference.fasta \
 -r reads.fastq \
 -i fastq \
 -o 2 \
 -s mapped_reads.bam
+
+#Header modifying
 samtools view -H mapped_reads.bam \
 | sed 's/SM:NOSM/SM:Sample1/' \
 | samtools reheader - mapped_reads.bam \
 > mapped_reads.reheaded.bam
-samtools sort mapped_reads.reheaded.bam \
-mapped_reads.reheaded.sorted
-samtools mpileup -uf reference.fasta \
-mapped_reads.reheaded.sorted.bam \
+
+#Bam file sorting
+samtools sort mapped_reads.reheaded.bam mapped_reads.reheaded.sorted
+#Variant detection
+samtools mpileup -uf reference.fasta mapped_reads.reheaded.sorted.bam \
 | bcftools call -cv \
 | vcfutils.pl varFilter > variants.samtools.vcf
+
